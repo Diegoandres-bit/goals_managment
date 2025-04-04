dbbbc65c27eed257d05b9920e84e8a0667664277
Diegoandres-bit <diegoandresosorio8@gmail.com>
2025-04-03 21:56:55 -0500

    propierties

---
+++ goals-management/src/main/java/com/example/goals_management/GoalsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.goals_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalsManagementApplication.class, args);
+	}
+
+}
